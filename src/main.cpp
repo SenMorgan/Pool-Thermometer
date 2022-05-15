df58c9d9ecac0295a522424f7343fff2da317b58
Sen Morgan <technars@gmail.com>
2022-05-15 18:37:22 +0200

    feat: Add pubsubclient.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
 #include "credentials.h"
 #include "def.h"
 
 const char ssid[] = WIFI_SSID;
 const char pass[] = WIFI_PASSWD;
+
+WiFiClient   espClient;
+PubSubClient client(espClient);
 
 ADC_MODE(ADC_VCC);
 
@@ -11,6 +17,10 @@ void setup()
 {
     Serial.begin(115200);
     pinMode(STATUS_LED, OUTPUT);
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, pass);
+    WiFi.hostname(HOSTNAME);
 }
 
 void loop()
